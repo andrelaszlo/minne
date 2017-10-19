6d8ca21294880b02c0b8dd7e7ce9eb73dfbf66b4
Narjis Hachilif <hnarjis@gmail.com>
2017-10-19 18:16:06 +0200

    Add InboxPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { InboxPage } from '../pages/inbox/inbox';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
@@ -21,8 +22,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Inbox', component: InboxPage },
+      { title: 'Archive', component: ListPage }
     ];
 
   }
