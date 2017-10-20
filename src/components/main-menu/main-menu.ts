43170731fb20f40a06a3578956eed2061c1cb925
André Laszlo <andre@laszlo.nu>
2017-10-20 23:13:46 +0200

    Add the add button

--- src/components/main-menu/main-menu.ts
+++ src/components/main-menu/main-menu.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AppModule } from '../../app/app.module';
 
 @Component({
   selector: 'main-menu',
@@ -8,6 +9,7 @@ export class MainMenuComponent {
 
   @Input()
   title: string;
+  appName: string = AppModule.applicationName;
 
   constructor() {
     this.title = '';
