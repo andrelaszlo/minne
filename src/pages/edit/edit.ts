b2d728e741f11c71f87ea662e41bb1724a4ebcf2
André Laszlo <andre@laszlo.nu>
2017-10-20 18:43:20 +0200

    Editing notes work

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ViewController, NavController, NavParams } from 'ionic-angular';
 import { FirebaseProvider } from '../../providers/firebase/firebase'
 
 /**
- * Generated class for the EditPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the EditPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
@@ -15,17 +15,22 @@ import { FirebaseProvider } from '../../providers/firebase/firebase'
   templateUrl: 'edit.html',
 })
 export class EditPage {
-  public note;
-  public key;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
-    this.note = navParams.get("note");
-    this.key = this.note.key;
-    delete this.note.key;
+  public note: any = {'content': null};
+  public key: any;
+  
+  constructor(
+    public viewCtrl: ViewController,
+    public navParams: NavParams,
+    public firebaseProvider: FirebaseProvider) {
+      this.note = navParams.get("note");
+      this.key = this.note.key;
+      delete this.note.key;
+    }
+    
+    saveNote() {
+      this.firebaseProvider.saveItem(this.key, this.note);
+      this.viewCtrl.dismiss();
+    }
+    
   }
-
-  saveNote() {
-    this.firebaseProvider.saveItem(this.key, this.note);
-  }
-
-}
+  
