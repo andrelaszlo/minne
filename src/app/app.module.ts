a0f51652380187d8fe9eca159fbec7112549970d
André Laszlo <andre@laszlo.nu>
2017-10-23 18:29:25 +0200

    Try a different UA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -79,7 +79,7 @@ const firebaseConfig = {
 export class AppModule {
   
   constructor(private userAgent: UserAgent) {
-    let userAgentString: string = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+    let userAgentString: string = 'Mozilla/5.0 Google';
     this.userAgent.set(userAgentString)
       .then((res: any) => console.log(res))
       .catch((error: any) => console.error(error));
