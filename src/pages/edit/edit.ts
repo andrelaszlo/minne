63432a2d9df1f1394f34e65a50753daf6126bb03
André Laszlo <andre@laszlo.nu>
2017-10-31 17:40:03 +0100

    Add close button and move save button in edit/add modal

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -5,24 +5,28 @@ import { FirebaseProvider } from '../../providers/firebase/firebase'
 @IonicPage()
 @Component({
   selector: 'page-edit',
-  templateUrl: 'edit.html',
+  templateUrl: '../add/add.html',
 })
 export class EditPage {
   public note: any = {'content': null};
   public id: string = null;
-  
+
   constructor(
+    public navCtrl: NavController,
     public viewCtrl: ViewController,
     public navParams: NavParams,
     public firebaseProvider: FirebaseProvider) {
       this.note = navParams.get("note");
       this.id = this.note.id;
-    }
-    
-    saveNote() {
-      this.firebaseProvider.saveItem(this.note.id, this.note);
-      this.viewCtrl.dismiss();
-    }
-    
   }
-  
+
+  saveNote() {
+    this.firebaseProvider.saveItem(this.note.id, this.note);
+    this.viewCtrl.dismiss();
+  }
+
+  dismiss() {
+    this.navCtrl.pop();
+  }
+
+}
