d638972d08cc60ca7ff806760253f05074071c90
André Laszlo <andre@laszlo.nu>
2017-11-02 18:56:32 +0100

    Fix Google login!
    Future reference:
    
    https://firebase.google.com/docs/auth/web/cordova
    http://ionicframework.com/docs/v1/guide/testing.html
    https://forum.ionicframework.com/t/android-device-console-log/86049/4
    https://javebratt.com/ionic-social-login-firebase/
    
    Eventually I figured out that I had the right plugins installed, but
    they were in a bad state. Reinstalled cordova-plugin-customurlscheme
    with the right url and it started loading!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,6 +61,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      console.log("Platform ready")
       moment.locale(this.configProvider.getLocale());
       this.statusBar.styleDefault();
       this.splashScreen.hide();
