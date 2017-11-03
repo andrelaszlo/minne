4c8e4b6bb92fcd6ba3c68c336eb6e307a5bc4d2b
André Laszlo <andre@laszlo.nu>
2017-11-03 14:53:53 +0100

    Try getting build info for easier debugging

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthProvider } from '../../providers/auth/auth';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { ConfigProvider } from '../../providers/config/config';
+import * as moment from 'moment-timezone';
+
+declare var BuildInfo: any;
 
 @IonicPage()
 @Component({
@@ -10,14 +13,21 @@ import { ConfigProvider } from '../../providers/config/config';
 })
 export class LoginPage {
   appName: string;
+  buildInfo: string = "Unknown version";
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private auth: AuthProvider,
     configProvider: ConfigProvider,
+    public platform: Platform,
   ) {
-    this.appName = configProvider.applicationName;    
+    this.platform.ready().then(() => {
+      if (this.platform.is('cordova')) {
+        this.buildInfo = `${BuildInfo.displayName} ${BuildInfo.version} ${BuildInfo.buildType} built ${moment(BuildInfo.buildDate).calendar()}`;
+      }
+    });
+    this.appName = configProvider.applicationName;
   }
 
   login(provider: string) {
