bdb95fd5df1b3b42b35887f5b45b9420d280aad4
André Laszlo <andre@laszlo.nu>
2017-11-09 18:25:42 +0100

    Notifications based on events

--- src/providers/notification/notification.ts
+++ src/providers/notification/notification.ts
@@ -3,6 +3,17 @@ import { Storage } from '@ionic/storage';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import 'rxjs/add/operator/map';
 import * as moment from 'moment-timezone';
+import { FirebaseProvider, Note } from '../../providers/firebase/firebase';
+import { AuthProvider } from '../../providers/auth/auth';
+import { Observable } from 'rxjs';
+
+/*
+  TODO
+  - Don't show a notification twice, even between restarts.
+  - Update when a note is added.
+  - Multiple notification times
+  - Multiple notification methods
+*/
 
 @Injectable()
 export class NotificationProvider {
@@ -10,10 +21,12 @@ export class NotificationProvider {
   private uniqueId: number = 0;
 
   constructor(
-    storage: Storage,
-    public localNotifications: LocalNotifications
+    public firebaseProvider: FirebaseProvider,
+    public localNotifications: LocalNotifications,
+    private authProvider: AuthProvider
   ) {
-    const EVERY_HOUR = 1000*60*60;
+    //const EVERY_HOUR = 1000*60*60;
+    const EVERY_HOUR = 10000;
 
     this.localNotifications.on("trigger", (notification) => console.log("*** Notification triggered", notification));
     this.localNotifications.on("schedule", (notification) => console.log("*** Notification scheduled", notification));
@@ -21,66 +34,43 @@ export class NotificationProvider {
     this.localNotifications.on("clear", (notification) => console.log("*** Notification cleared", notification));
     this.localNotifications.on("click", (notification) => this.localNotifications.clear(notification.id));
 
-
-    this.refreshNotifications();
-    setInterval(() => this.refreshNotifications(), EVERY_HOUR);
-
+    this.authProvider.getUserPromise().then( user => {
+      this.refreshNotifications();
+      setInterval(() => this.refreshNotifications(), EVERY_HOUR);
+    });
   }
 
   private getUniqueId(): number {
     return ++this.uniqueId;
   }
 
-  private refreshNotifications() {
+  public refreshNotifications() {
     console.log("Refereshing notifications");
+    this.localNotifications.cancelAll();
     // Get current notifications from local storage
-    let currentNotifications = this.getNotificationsFromStorage();
-    for(let note of currentNotifications) {
-      this.localNotifications.schedule({
-        id: this.getUniqueId(),
-        at: moment(note.date).toDate(),
-        text: note.content
-      });
-    }
+    let currentNotifications: Promise<number[]> = this.localNotifications.getAllIds();
     // Get all events for next 7 days
     let upcomingEvents = this.getUpcomingEvents();
-    // Add any new notifications
-    // Refresh regularly
-  }
-
-  private getNotificationsFromStorage() {
-    return [
-      /*
-      {
-        content: 'already 1',
-        date: moment().subtract(10, 'seconds')
-      },
-      {
-        content: 'already 2',
-        date: moment().subtract(1, 'years')
-      },
-      {
-        content: '5 seconds',
-        date: moment().add(5, 'seconds')
-      },
-      {
-        content: '10 seconds',
-        date: moment().add(10, 'seconds').format()
-      },
-      {
-        content: '15 seconds',
-        date: moment().add(15, 'seconds').format()
+    upcomingEvents.take(1).forEach((items: any) => {
+      console.log("Checking notifications for upcoming events", items);
+      // Add any new notifications
+      for (let item of items) {
+        this.addNotification(item);
       }
-      */
-    ]
+    });
   }
 
-  private getUpcomingEvents() {
-
+  private getUpcomingEvents(): Observable<Note[]> {
+    return this.firebaseProvider.getUpcomingItems();
   }
 
-  public addNotification(note: any) {
-
+  private addNotification(note: any) {
+    let itemId = this.getUniqueId();
+    this.localNotifications.schedule({
+      id: itemId,
+      at: moment(note.date).subtract(10, 'minutes').toDate(),
+      text: note.content,
+    });
   }
 
 }
