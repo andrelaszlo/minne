4e6c57d4865360491268d78a2f12de63fe13de12
André Laszlo <andre@laszlo.nu>
2017-11-10 16:21:44 +0100

    WIP :(

--- src/providers/notification/notification.ts
+++ src/providers/notification/notification.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { LocalNotifications } from '@ionic-native/local-notifications';
+import { ILocalNotification, LocalNotifications } from '@ionic-native/local-notifications';
 import 'rxjs/add/operator/map';
 import * as moment from 'moment-timezone';
 import { FirebaseProvider, Note } from '../../providers/firebase/firebase';
 import { AuthProvider } from '../../providers/auth/auth';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 /*
   TODO
@@ -23,6 +23,7 @@ export class NotificationProvider {
   constructor(
     public firebaseProvider: FirebaseProvider,
     public localNotifications: LocalNotifications,
+    public storage: Storage,
     private authProvider: AuthProvider
   ) {
     //const EVERY_HOUR = 1000*60*60;
@@ -55,7 +56,8 @@ export class NotificationProvider {
       console.log("Checking notifications for upcoming events", items);
       // Add any new notifications
       for (let item of items) {
-        this.addNotification(item);
+        new StoredNotification(item, this);
+        //this.addNotification(item);
       }
     });
   }
@@ -73,4 +75,94 @@ export class NotificationProvider {
     });
   }
 
+}
+
+class StoredNotification {
+  private notificationProvider: NotificationProvider;
+  private note: Note;
+  private storage: Storage;
+  
+  constructor(
+    note: Note,
+    notificationProvider: NotificationProvider
+  ) {
+    this.notificationProvider = notificationProvider;
+    this.storage = notificationProvider.storage;
+
+    this.note = note;
+    this.updateNotifications();
+  }
+
+  private updateNotifications() {
+    let type = 'default'; // TODO see below
+    let notifications = this.getNotifications().then(notifications => {
+      let found = false;
+      for (let notificationItem of notifications) {
+        if (notificationItem.type == type) {
+          found = true;
+        }
+      }
+      if (!found) {
+        this.addNotification(this.note);
+      }
+    });
+  }
+
+  private getNotifications(): Promise<any[]> {
+    let subject: Subject<any> = new Subject();
+    
+    this.notificationMapping()
+      .then(notificationIds => {
+        notificationIds.forEach(([id, type]) => {
+            this.notificationProvider.localNotifications.get(id)
+              .then(notification => subject.next({id, notification, type}))
+        })
+        subject.complete();
+      })
+    
+    subject.forEach(item => console.log("subject item", item));
+    return subject.toArray().toPromise();
+  }
+
+  private getReferenceString(): string {
+    if (!this.note.id) {
+      console.log("Unknown note id", this.note);
+      throw new Error("Note id unknown");
+    }
+    return 'notification-mapping-' + this.note.id;
+  }
+
+  private notificationMapping(): any {
+    return new Promise((resolve, reject) => {
+      this.storage
+        .get(this.getReferenceString())
+        .then(value =>
+          resolve(value || []));
+    });
+  }
+
+  private addNotificationToMapping(notificationId) {
+    let item = {
+      id: notificationId,
+      type: 'default' // TODO: the plan is to use this to have a name for each type of notification, eg "10 minutes" or "default". Not sure yet.
+    };
+    this.storage.get(this.getReferenceString()).then(list => {
+      list = (list || []).push(item)
+      this.storage.set(this.getReferenceString(), list);
+    });
+  }
+
+  private getUniqueId(): number {
+    return Date.now() * 1000000 + Math.floor((Math.random() * 1000000));
+  }
+
+  private addNotification(note: Note) {
+    let notificationId = this.getUniqueId();
+    this.notificationProvider.localNotifications.schedule({
+      id: notificationId,
+      at: moment(note.date).subtract(10, 'minutes').toDate(),
+      text: note.content,
+    });
+    this.addNotificationToMapping(notificationId);
+  }
 }
