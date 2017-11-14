c2d2b2757e1050a522a63a7cd386e00179d6b9f8
André Laszlo <andre@laszlo.nu>
2017-11-14 11:40:59 +0100

    Clean up

--- src/providers/notification/notification.ts
+++ src/providers/notification/notification.ts
@@ -11,7 +11,6 @@ import { PersistentObject } from '../../lib/collection-helpers';
 /*
   TODO
   - Don't show a notification twice, even between restarts.
-  - Update when a note is added.
   - Multiple notification times
   - Multiple notification methods
 */
@@ -27,10 +26,7 @@ export class NotificationProvider {
     public storage: Storage,
     private authProvider: AuthProvider
   ) {
-    //const EVERY_HOUR = 1000*60*60;
-    const EVERY_HOUR = 60000;
-    console.log("storage", storage);
-
+    const EVERY_HOUR = 1000*60*60;
 
     this.localNotifications.on("trigger", (notification) => console.log("*** Notification triggered", notification));
     this.localNotifications.on("schedule", (notification) => console.log("*** Notification scheduled", notification));
@@ -41,43 +37,29 @@ export class NotificationProvider {
     this.authProvider.getUserPromise().then( user => {
       this.refreshNotifications();
       setInterval(() => this.refreshNotifications(), EVERY_HOUR);
+      this.getUpcomingEvents().forEach(items => {
+        console.log("Events updated", items);
+        this.refreshNotificationItems(items);
+      });
     });
-  }
-
-  private getUniqueId(): number {
-    return ++this.uniqueId;
   }
 
   public refreshNotifications() {
     console.log("Refereshing notifications");
-    this.localNotifications.cancelAll();
-    // Get current notifications from local storage
-    let currentNotifications: Promise<number[]> = this.localNotifications.getAllIds();
-    // Get all events for next 7 days
-    let upcomingEvents = this.getUpcomingEvents();
-    upcomingEvents.take(1).forEach((items: any) => {
-      console.log("Checking notifications for upcoming events", items);
-      // Add any new notifications
-      for (let item of items) {
-        new StoredNotification(item, this);
-        //this.addNotification(item);
-      }
-    });
+    this.getUpcomingEvents().take(1).forEach(items => this.refreshNotificationItems(items));
+  }
+
+  private refreshNotificationItems(items: any) {
+    console.log("Checking notifications for upcoming events", items);
+    this.localNotifications.cancelAll();      
+    for (let item of items) {
+      new StoredNotification(item, this);
+    }
   }
 
   private getUpcomingEvents(): Observable<Note[]> {
     return this.firebaseProvider.getUpcomingItems();
   }
-
-  private addNotification(note: any) {
-    let itemId = this.getUniqueId();
-    this.localNotifications.schedule({
-      id: itemId,
-      at: moment(note.date).subtract(10, 'minutes').toDate(),
-      text: note.content,
-    });
-  }
-
 }
 
 class StoredNotification {
@@ -149,21 +131,12 @@ class StoredNotification {
     return obs.toArray().toPromise();
   }
 
-  private getReferenceString(): string {
-    if (!this.note.id) {
-      console.log("Unknown note id", this.note);
-      throw new Error("Note id unknown");
-    }
-    return 'notification-mapping-' + this.note.id;
-  }
-
   private addNotificationToMapping(notificationId) {
     let item = {
       id: notificationId,
       type: 'default' // TODO: the plan is to use this to have a name for each type of notification, eg "10 minutes" or "default". Not sure yet.
     };
     this.notificationMapping.do(ids => {
-      console.log("Setting", ids, item);
       ids.push(item);
       return ids;
     });
@@ -174,7 +147,6 @@ class StoredNotification {
   }
 
   private addNotification(note: Note, reschedule=false, id:number = null) {
-    console.log("add notification", reschedule, id);
     let notificationId = id || this.getUniqueId();
     this.notificationProvider.localNotifications.schedule({
       id: notificationId,
