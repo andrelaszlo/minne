f9052ec5cefff983ba6c38768e1f6f2564f386d0
André Laszlo <andre@laszlo.nu>
2017-11-16 14:42:17 +0100

    Add error handling on all promises

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -38,7 +38,7 @@ export class AuthProvider {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        console.log(token, user);
+        console.log("sign in info", token, user);
       }).catch(function(error) {
         // Handle Errors here.
         console.log(error.message);
