9715bded1d56daa29061279c94bf5a62b5fae2fe
Narjis Hachilif <hnarjis@gmail.com>
2017-11-28 13:39:33 +0100

    Add directly to a day

--- src/pages/goals/goals.ts
+++ src/pages/goals/goals.ts
@@ -35,7 +35,7 @@ export class GoalsPage {
     firebaseProvider.getSortedItems().forEach(items => {
       var result = {};
       for (let item of items) {
-        let key = moment(item.date).format('ddd, MMM DD YYYY');
+        let key = moment(item.date).startOf('day').format();
         if (!result[key]) {
           result[key] = {items: [], hours: this.firebaseProvider.getFreeHours(item.date)};
         }
@@ -104,8 +104,10 @@ export class GoalsPage {
     modal.present();
   }
 
-  addNewNote() {
-    let modal = this.modalCtrl.create(AddPage);
+  addNewNote(day) {
+    let modal = this.modalCtrl.create(AddPage, {
+      startDay: day
+    });
     modal.present();
   }
 
@@ -118,26 +120,9 @@ export class GoalsPage {
     var now = moment().startOf('day');
     var lastDate = moment().add(1, 'year').startOf('day');
     while (now.isSame(lastDate) || now.isBefore(lastDate)) {
-      days.push(now.clone().format('ddd, MMM DD YYYY'));
+      days.push(now.clone().format());
       now.add(1, 'day');
     }
     return days;
   }
-
-  private getDayName(note) {
-    return moment(note.date)
-    .calendar(null, {
-      sameDay: '[Today]',
-      nextDay: '[Tomorrow]',
-      nextWeek: 'dddd',
-      sameElse: function(date) {
-        let endOfYear = moment().endOf("year");
-        if (this.isAfter(endOfYear)) {
-          return 'Y';
-        }
-        return 'MMMM';
-      }
-    });
-  }
-
 }
